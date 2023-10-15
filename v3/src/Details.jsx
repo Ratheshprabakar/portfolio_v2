0b10f9fd96e3a211f579ec1f7a772de29b59f190
RatheshPrabakar <ratheshprabakar@gmail.com>
2023-10-15 21:53:47 +0530

    Initial Commit

---
+++ v3/src/Details.jsx
@@ -0,0 +1,13 @@
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Rathesh Prabakar",
+  tagline: "I build things for web",
+//   img: profile,
+  about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+};
+
+export const SOCIAL_MEDIA_URL = {
+    LINKEDIN : "https://linkedin.com/in/Ratheshprabakar",
+    GITHUB : "https://github.com/Ratheshprabakar"
+}
